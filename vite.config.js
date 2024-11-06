172d4121255bb6f7cb398f0d941ddf328c0886f5
Crow, Michael C <crowmc@ornl.gov>
2024-11-06 17:09:38 -0500

    renamed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/word-search',
+  base: '/word-search-ui',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
