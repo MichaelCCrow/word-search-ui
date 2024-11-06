0c5459e2c5e9ecbf5642eeff95f8cc273dc9d3bf
Crow, Michael C <crowmc@ornl.gov>
2024-11-06 17:00:27 -0500

    wrapped text fields in defaults provider

--- src/components/WordSearch.vue
+++ src/components/WordSearch.vue
@@ -16,6 +16,12 @@ const withinMax = computed(() => beg.value.length + mid.value.length + end.value
 const results = computed(() => beg.value||mid.value||end.value ? words.filter(word => word.startsWith(beg.value) && word.substring(beg.value.length).includes(mid.value) && word.endsWith(end.value)) : [])
 
 const errors = computed(() => withinMax.value || `Cannot input more letters than max length: ${len.value}`)
+const textFieldDefaults = ref({
+  VTextField: {
+    errorMessages: errors,
+    class: 'v-col-4',
+  },
+})
 
 const query = ref('')
 const anything = computed(() => query.value ? words.filter(word => [...query.value].every(q=>word.includes(q))) : [])
@@ -27,17 +33,19 @@ const anything = computed(() => query.value ? words.filter(word => [...query.val
     <v-card>
       <v-card-title class="v-row">
         <h2 class="v-col-3">Word Search</h2>
-        <v-number-input v-model.number="len" label="Word Length" class="v-col-3 offset-6" />
+        <v-col cols="1" offset="2" class="mt-1">
+          <v-btn v-tooltip="'Clear'" color="warning" icon="mdi-nuke" @click="[beg='', mid='', end='']" />
+        </v-col>
+        <v-number-input v-model.number="len" label="Word Length" class="v-col-3 offset-3" />
       </v-card-title>
       <v-divider/>
       <v-card-item>
         <v-row>
-          <v-text-field v-model.trim="beg" label="Beginning Letters" :error-messages="errors" :maxlength="len+beg.length-count" class="v-col-4" append-inner-icon="mdi-arrow-expand-right" />
-          <v-text-field v-model.trim="mid" label="Middle Letters"    :error-messages="errors" :maxlength="len+mid.length-count" class="v-col-4" append-inner-icon="mdi-arrow-expand-horizontal" />
-          <v-text-field v-model.trim="end" label="Ending Letters"    :error-messages="errors" :maxlength="len+end.length-count" class="v-col-3" prepend-inner-icon="mdi-arrow-expand-left" />
-          <v-col cols="1" class="mt-1">
-            <v-btn v-tooltip="'Clear'" color="warning" icon="mdi-nuke" @click="[beg='', mid='', end='']" />
-          </v-col>
+          <v-defaults-provider :defaults="textFieldDefaults">
+            <v-text-field v-model.trim="beg" label="Beginning Letters" :maxlength="len+beg.length-count" append-inner-icon="mdi-arrow-expand-right" />
+            <v-text-field v-model.trim="mid" label="Middle Letters"    :maxlength="len+mid.length-count" append-inner-icon="mdi-arrow-expand-horizontal" />
+            <v-text-field v-model.trim="end" label="Ending Letters"    :maxlength="len+end.length-count" prepend-inner-icon="mdi-arrow-expand-left" />
+          </v-defaults-provider>
         </v-row>
       </v-card-item>
     </v-card>
